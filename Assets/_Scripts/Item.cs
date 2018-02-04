86e356f7b5761c971aad189b400de13fa9e8285e
James Davis <Jamesmdavis95@gmail.com>
2018-02-04 12:35:38 -0800

    Item
    James Davis added the Item script which gives an item a certain amount of points.

---
+++ Assets/_Scripts/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    [SerializeField]
+    int points;
+
+    public int Points
+    {
+        get
+        {
+            return points;
+        }
+    }
+
+    public void AddPoints()
+    {
+
+    }
+}
