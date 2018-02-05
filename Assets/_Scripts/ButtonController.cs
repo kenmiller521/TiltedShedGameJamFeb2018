e1c23c4008e3aa2200b8c6c51d61fb9deb7ec9bf
Stanley Ung <StanleyUng167@gmail.com>
2018-02-04 19:49:22 -0800

    Script fixes

--- Assets/_Scripts/ButtonController.cs
+++ Assets/_Scripts/ButtonController.cs
@@ -5,81 +5,60 @@ using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour {
 
-    //public KeyCode key;
+    // Button References
     public Image zButton, xButton, cButton;
     public Image bButton, nButton, mButton;
-    
-    public Color newColor;
 
-    //References to the Player Animations
-    public Animator player1Anim;
-    public Animator player2Anim;
+    public bool InputActive = true;     // Can we press the buttons?
 
 	private void Update () {
-        // In Game Buttons
+		// In Game Buttons
+        if (InputActive) { 
+            if (Input.GetKeyDown(KeyCode.Z)) 
+            {
+                Debug.Log("Z");
+                zButton.color = Color.gray;
+            } else if (Input.GetKeyDown(KeyCode.X)) 
+            {
+                Debug.Log("X");
+                xButton.color = Color.gray;
+            } else if (Input.GetKeyDown(KeyCode.C)) 
+            { 
+                Debug.Log("C");
+                cButton.color = Color.gray;
+            } else if (Input.GetKeyDown(KeyCode.B)) 
+            {
+                Debug.Log("B");
+                bButton.color = Color.gray;
+            } else if (Input.GetKeyDown(KeyCode.N)) 
+            { 
+                Debug.Log("N");
+                nButton.color = Color.gray;
+            } else if (Input.GetKeyDown(KeyCode.M)) 
+            {
+                Debug.Log("M");
+                mButton.color = Color.gray;
+            }
 
-        bool player1Swipe = false;
-        bool player2Swipe = false;
-
-        if (Input.GetKeyDown(KeyCode.Z)) 
-        {
-            Debug.Log("Z");
-            zButton.color = Color.gray;
-            player1Swipe = true;
-        } else if (Input.GetKeyDown(KeyCode.X)) 
-        {
-            Debug.Log("X");
-            xButton.color = Color.gray;
-            player1Swipe = true;
-        } else if (Input.GetKeyDown(KeyCode.C)) 
-        { 
-            Debug.Log("C");
-            cButton.color = Color.gray;
-            player1Swipe = true;
-        } else if (Input.GetKeyDown(KeyCode.B)) 
-        {
-            Debug.Log("B");
-            bButton.color = Color.gray;
-            player2Swipe = true;
-        } else if (Input.GetKeyDown(KeyCode.N)) 
-        { 
-            Debug.Log("N");
-            nButton.color = Color.gray;
-            player2Swipe = true;
-        } else if (Input.GetKeyDown(KeyCode.M)) 
-        {
-            Debug.Log("M");
-            mButton.color = Color.gray;
-            player2Swipe = true;
-        }
-
-        if(player1Swipe)
-        {
-            player1Anim.SetTrigger("Swiped");
-        }
-        if(player2Swipe)
-        {
-            player2Anim.SetTrigger("Swiped");
-        }
-
-        if (Input.GetKeyUp(KeyCode.Z)) 
-        {
-            zButton.color = Color.white;
-        } else if (Input.GetKeyUp(KeyCode.X)) 
-        {
-            xButton.color = Color.white;
-        } else if (Input.GetKeyUp(KeyCode.C)) 
-        { 
-            cButton.color = Color.white;
-        } else if (Input.GetKeyUp(KeyCode.B)) 
-        {
-            bButton.color = Color.white;
-        } else if (Input.GetKeyUp(KeyCode.N)) 
-        { 
-            nButton.color = Color.white;
-        } else if (Input.GetKeyUp(KeyCode.M)) 
-        {
-            mButton.color = Color.white;
+            if (Input.GetKeyUp(KeyCode.Z)) 
+            {
+                zButton.color = Color.white;
+            } else if (Input.GetKeyUp(KeyCode.X)) 
+            {
+                xButton.color = Color.white;
+            } else if (Input.GetKeyUp(KeyCode.C)) 
+            { 
+                cButton.color = Color.white;
+            } else if (Input.GetKeyUp(KeyCode.B)) 
+            {
+                bButton.color = Color.white;
+            } else if (Input.GetKeyUp(KeyCode.N)) 
+            { 
+                nButton.color = Color.white;
+            } else if (Input.GetKeyUp(KeyCode.M)) 
+            {
+                mButton.color = Color.white;
+            }
         }
 	}
 }
